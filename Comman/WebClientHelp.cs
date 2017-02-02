bef3477faf5c722566bfc53b62ac118dcf9c8f94
rd00009 <rohitkumar925@gmail.com>
2017-02-02 18:54:20 +0530

    First code checkin

---
+++ Comman/WebClientHelp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace BluSignalHelpMethod
+{
+  public  class WebClientHelp
+    {
+        public string GetWebClientResponse(string webClientAddress)
+        {
+            string s = "";
+            using (WebClient webClient = new WebClient())
+            {
+                 s = webClient.DownloadString(webClientAddress);
+            }
+            return s;
+        }
+    }
+}
